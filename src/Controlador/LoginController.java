4689978bfdaf0e9f43dde65b01c1369a29d18dde
san1tago <santiagosoper@unisabana.edu.co>
2024-11-10 22:42:31 -0500

    comentario de prueba2

--- src/Controlador/LoginController.java
+++ src/Controlador/LoginController.java
@@ -1,6 +1,8 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+
+sanitago uwu
  */
 
 package Controlador;
